61bec020da669a61d5f83e1dc38775b73907e9d3
lorenzo-neri <lorenzo21neri@gmail.com>
2023-10-12 17:16:32 +0200

    add card in page with data

--- index.php
+++ index.php
@@ -21,10 +21,30 @@ include __DIR__ . '/assets/layout/head.php'
 
 
 <div id="app">
+    <header class="p-3 text-white d-flex align-items-center">
+        <h1 class="ms-5">{{title}}</h1>
+    </header>
 
-    <h1>{{title}}</h1>
+    <main>
 
-    <ul>
+        <div class="container">
+            <div class="row">
+                <div v-for="(disk, index) in disks" :key="disk + index" class="col-4">
+                    <div class="card py-4 px-5 m-4 text-white">
+                        <img :src="disk.poster" class="card-img-top" :alt="disk.title">
+                        <div class="card-body text-center">
+                            <h3 class="card-title">{{disk.title}}</h3>
+                            <p class="card-text">{{disk.author}}</p>
+                            <h3>{{disk.year}}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <!-- <ul>
         <li v-for="(disk, index) in disks" :key="disk + index">
             {{disk.title}}
             <ul>
@@ -33,7 +53,7 @@ include __DIR__ . '/assets/layout/head.php'
                 <li>{{disk.genre}}</li>
             </ul>
         </li>
-    </ul>
+    </ul> -->
 
 </div>
 
